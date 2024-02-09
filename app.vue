1c48fd12788afae3be23a11815a7d50828838591
pedraal <pro@pedraal.fr>
2024-02-09 21:27:10 +0100

    feat(ui): setup libs

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+useHead({
+  title: 'ChipChat',
+  meta: [
+    {
+      name: 'description',
+      content: 'ChipChat is a chat application',
+    },
+  ],
+  bodyAttrs: {
+    class: 'bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100',
+  },
+})
+
+const colorMode = useColorMode()
+
+function toggleColorMode() {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-full min-h-screen flex flex-col">
+    <nav class="flex items-center justify-end gap-4 container mx-auto p-2">
+      <button class="nav-item" @click="toggleColorMode">
+        <Icon name="heroicons:sun" class="w-6 h-6" />
+      </button>
+      <a href="https://github.com/pedraal/chipchat" class="nav-item">
+        <Icon name="fa-brands:github" class="w-6 h-6" />
+      </a>
+    </nav>
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="grow w-full flex justify-center items-center">
+    <div class="text-center">
+      <p class="text-3xl font-medium">
+        Welcome to
+      </p>
+      <h1 class="text-4xl text-amber-400 dark:text-amber-500 font-bold flex items-center gap-2">
+        <Icon name="fluent-emoji-high-contrast:potato" />
+        ChipChat
+      </h1>
+    </div>
+  </div>
+</template>
