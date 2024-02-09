c12f554b1744050767e1e7d841f1e7667b87dc38
pedraal <pro@pedraal.fr>
2024-02-09 21:37:16 +0100

    feat(ui): small touch of design

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,25 @@
 
 <template>
   <div class="grow w-full flex justify-center items-center">
-    <div class="text-center">
-      <p class="text-3xl font-medium">
-        Welcome to
-      </p>
-      <h1 class="text-4xl text-amber-400 dark:text-amber-500 font-bold flex items-center gap-2">
-        <Icon name="fluent-emoji-high-contrast:potato" />
-        ChipChat
-      </h1>
+    <div class="flex flex-col items-center gap-6">
+      <div class="text-center">
+        <p class="text-3xl font-medium">
+          Welcome to
+        </p>
+        <h1 class="text-4xl font-bold flex items-center gap-2">
+          <Icon name="fluent-emoji-high-contrast:potato" class="text-amber-400 dark:text-amber-600" />
+          <span class=" bg-gradient-to-r primary-gradient bg-clip-text text-transparent">
+            ChipChat
+
+          </span>
+        </h1>
+      </div>
+      <div class="relative group">
+        <div class="absolute -inset-0.5 group-hover:blur-lg transition-all bg-gradient-to-r primary-gradient rounded-lg blur animate-tilt" />
+        <button class="relative px-7 py-4 bg-zinc-100 dark:bg-zinc-900 rounded-lg leading-none">
+          Continue as guest
+        </button>
+      </div>
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ export default {
   darkMode: 'class',
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        tilt: 'tilt 10s infinite linear',
+      },
+      keyframes: {
+        tilt: {
+          '0%, 50%, 100%': {
+            transform: 'rotate(0deg)',
+          },
+          '25%': {
+            transform: 'rotate(10deg)',
+          },
+          '75%': {
+            transform: 'rotate(-10deg)',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
