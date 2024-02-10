3ca205483932d33818cc353e12d2fade22be80ea
pedraal <pro@pedraal.fr>
2024-02-10 03:09:48 +0100

    fix(ui): input border and type issue

--- components/form/Input.vue
+++ components/form/Input.vue
@@ -7,9 +7,9 @@ defineProps<{
 
 defineEmits(['update:modelValue'])
 
-const injectedName = inject('name')
+const injectedName = inject<string>('name')
 </script>
 
 <template>
-  <input :value="modelValue" :type="type || 'text'" :name="name || injectedName" class="peer bg-transparent ring-1 ring-gray-900 dark:ring-gray-100 rounded-lg py-1 px-2 focus:!ring-primary-500 focus:ring-2 outline-none" @input="$emit('update:modelValue')">
+  <input :value="modelValue" :type="type || 'text'" :name="name || injectedName" class="peer bg-transparent ring-1 ring-gray-600 dark:ring-gray-400 rounded-lg py-1 px-2 focus:!ring-primary-500 focus:ring-2 outline-none" @input="$emit('update:modelValue')">
 </template>
